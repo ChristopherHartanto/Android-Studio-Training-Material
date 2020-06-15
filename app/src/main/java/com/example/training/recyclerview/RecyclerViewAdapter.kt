c7dac0ddc8ca8102584aa775c25c9f17eaf50e64
ChristopherHartanto <christopherhartanto999gmail.com>
2020-06-15 22:41:09 +0700

    material

--- app/src/main/java/com/example/training/recyclerview/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/training/recyclerview/RecyclerViewAdapter.kt
@@ -4,29 +4,36 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.training.R
 import kotlinx.android.synthetic.main.activity_home.view.*
 
-class RecyclerViewAdapter(val context: Context) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        ViewHolder(LayoutInflater.from(context).inflate(R.layout.activity_main,parent,false))
+class RecyclerViewAdapter (val context: Context, val items: List<String>, val itemImages: List<Int>)
+    : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolderA>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolderA{
+        return ViewHolderA(LayoutInflater.from(context).inflate(R.layout.item_rows, parent, false))
+    }
 
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return items.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: ViewHolderA, position: Int) {
+        holder.bindItem(items[position], itemImages[position])
     }
 
+    class ViewHolderA(view: View) : RecyclerView.ViewHolder(view){
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val ivItem = view.findViewById<ImageView>(R.id.ivItem)
+        val tvitem = view.findViewById<TextView>(R.id.tvItem)
 
-        fun bindItems(){
-
+        fun bindItem(nama: String, image: Int){
+            tvitem.text = nama
+            ivItem.setBackgroundResource(image)
         }
     }
-
 }
